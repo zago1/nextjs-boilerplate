5dfd5798e74b51ce9bd84749ca1cb9c3c57d9370
Igor Massaro <igorzagomassaro@gmail.com>
2024-07-23 16:36:53 -0300

    write first test; fix jest config

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { preset: ['next/babel'] }]
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
   }
 }
